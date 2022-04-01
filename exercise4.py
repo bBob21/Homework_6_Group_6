edadac0a564c60897ca23e9a0f683cfb3cfaef41
Sreng <kimsrengg@gmail.com>
2022-04-01 12:09:50 +0700

    Finished state

--- exercise4.py
+++ exercise4.py
@@ -1,3 +1,15 @@
 #Exercise 4
 
-print("Heya")
+list = [1,2,3,4,5]
+
+def remove_all_after(array,number):
+    print(f"hi{array}")
+    #check each elements in the array
+    for element in array:
+
+        while element == number:
+            array.pop()
+
+    return array
+
+print(remove_all_after(list,3))
