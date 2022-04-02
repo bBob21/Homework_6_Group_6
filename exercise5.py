150f3e1a6251cbecf31b293a4d3756b835ee96d9
bBob21 <102711034+bBob21@users.noreply.github.com>
2022-04-02 13:58:11 +0700

    changed text input/output

--- exercise5.py
+++ exercise5.py
@@ -1,11 +1,10 @@
 # Exercise 5
 
-print("Howdy")
 def backward_string_by_word(text: str):
     result = ' '.join([x[::-1] for x in text.split(' ')])
     return result
 
 
 if __name__ == '__main__':
-    print("howdy")
-    print(backward_string_by_word('howdy'))
+    print("Hello World")
+    print(backward_string_by_word('Hello World'))
