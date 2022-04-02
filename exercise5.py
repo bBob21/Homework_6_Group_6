c83217847a4ed4bca7bc58e80a1de175ad556781
bBob21 <102711034+bBob21@users.noreply.github.com>
2022-04-02 22:19:37 +0700

    Added explanation for the one line code, a bit confusing

--- exercise5.py
+++ exercise5.py
@@ -1,7 +1,18 @@
 # Exercise 5
 
 def backward_string_by_word(text: str):
+    
     result = ' '.join([x[::-1] for x in text.split(' ')])
+    
+    # explanation:
+        # for x in text.split(' '):
+            # takes the string input and converts it into a list using .split(' ')
+            # then make a for loop with x as the elements of the list
+        # ' '.join([x[::-1]]):
+            # x[::-1] means look at the letters in x backwards
+            # if x = Hello, it would look like olleH
+            # and combines each reversed word with a space using ' '.join()
+            
     return result
 
 
