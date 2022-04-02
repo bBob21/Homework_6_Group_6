f1fda1579f57ce802b8d7f71095ba16a6ad9c450
bBob21 <102711034+bBob21@users.noreply.github.com>
2022-04-02 22:05:36 +0700

    Added a list and print line

--- exercise2.py
+++ exercise2.py
@@ -1,6 +1,11 @@
 #Exercise 2
+
+list = [1,2,3,4]
+
 def index_nthpower(array, n):
     if(n>len(array)):
         return -1
     else:    
         return array[n]**n
+
+print(index_nthpower(list,2))
